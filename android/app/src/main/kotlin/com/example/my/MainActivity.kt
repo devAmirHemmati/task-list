5e4d7eb7aa6b7940a0ee205959b57fbb2a016be5
Amir <dev.amirhemmati1382@gmail.com>
2021-07-25 01:57:07 +0430

    initial task list manager flutter application

---
+++ android/app/src/main/kotlin/com/example/my/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Text("Hello Task List Manager App"),
+      ),
+    );
+  }
+}
